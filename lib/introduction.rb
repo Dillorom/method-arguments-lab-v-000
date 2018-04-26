482aaf63620de230802c3c184182ec5ce747242c
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-04-26 20:56:46 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,5 @@
 def introduction(name)
+<<<<<<< HEAD
   puts "Hi, my name is #{name}."
 end
 
@@ -6,3 +7,6 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
   
+=======
+  puts "Hi, my name is Josh.n"
+>>>>>>> f164492eed2126bd4271eb80697e983682322ace
